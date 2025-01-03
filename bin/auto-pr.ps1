1678d892ce653b1ddf626f385c04715323f2adfe
Yang Jeong Hun <onyxclover9931@gmail.com>
2025-01-03 14:05:23 +0900

    bucket: Rename scoop-bucket to frostbite
    Signed-off-by: Yang Jeong Hun <onyxclover9931@gmail.com>

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "Nevuly/scoop-bucket:master"
+    [String]$upstream = "Nevuly/frostbite:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
